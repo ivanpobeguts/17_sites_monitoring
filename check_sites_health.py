550198052a8dcf1e9d8f4917b957c84d4c578d59
Pobeguts Ivan <Ivan.Pobeguts@aton.ru>
2018-06-22 18:17:41 +0300

    add input urls from file add check is url OK, add expiration date check

--- check_sites_health.py
+++ check_sites_health.py
@@ -1,11 +1,55 @@
+import requests
+import argparse
+from datetime import datetime, date, timedelta
+import os
+
+
 def load_urls4check(path):
-    pass
+    urls = []
+    with open(path, 'r') as urls_file:
+        for line in urls_file:
+            urls.append(line.strip())
+    return urls
+
 
 def is_server_respond_with_200(url):
-    pass
+    try:
+        response = requests.get(url)
+    except (requests.exceptions.ConnectionError,
+            requests.exceptions.ProxyError):
+        return False
 
-def get_domain_expiration_date(domain_name):
-    pass
+    return response.ok
+
+
+def get_domain_expiration_date(url):
+    return datetime(2018, 6, 26, 0, 0)
+
+
+def count_days_before_expiration(expiration_date):
+    delta = expiration_date.date() - date.today()
+    return delta >= timedelta(days=30)
+
+
+def get_parser_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--path',
+        '-p',
+        help='path to file with urls for check',
+    )
+    args = parser.parse_args()
+    if not os.path.isfile(args.path):
+        parser.error(
+            "File doesn't exist"
+        )
+    return parser.parse_args()
+
 
 if __name__ == '__main__':
-    pass
+    args = get_parser_args()
+    urls = load_urls4check(args.path)
+    print(urls)
+    for url in urls:
+        print(is_server_respond_with_200(url))
+        print(count_days_before_expiration(get_domain_expiration_date(url)))
